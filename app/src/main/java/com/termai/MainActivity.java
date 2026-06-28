69f1acef8f70f95dd821f3c5bccd864791a7be8e
Mzro-349 <rr10bb20@gmail.com>
2026-06-28 15:58:50 +0300

    Fix: setFullscreen NullPointerException - use post()

--- app/src/main/java/com/termai/MainActivity.java
+++ app/src/main/java/com/termai/MainActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
 import android.view.WindowManager;
 import android.webkit.*;
 import android.os.Build;
